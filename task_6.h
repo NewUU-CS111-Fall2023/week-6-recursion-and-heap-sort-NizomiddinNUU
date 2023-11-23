0bb594234e3d231c736857bee43dddd9bb17fa5f
NizomiddinNUU <130057409+NizomiddinNUU@users.noreply.github.com>
2023-11-23 23:54:15 +0500

    task_6.h

---
+++ task_6.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void findSecondLargest(int& largest, int& secondLargest) {
+    int num;
+    cin >> num;
+
+    if (num == 0) {
+        return;
+    }
+
+    if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+    } else if (num > secondLargest && num != largest) {
+        secondLargest = num;
+    }
+
+    findSecondLargest(largest, secondLargest);
+}
+
+int main() {
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    findSecondLargest(largest, secondLargest);
+
+    cout << secondLargest << endl;
+
+    return 0;
+}
